07b8877faf54f5012d937e19678d566c745b81eb
Sangani Ajay <devankshu@pureites.com>
2022-04-07 18:09:32 +0530

    full modification

--- src/index.js
+++ src/index.js
@@ -1,58 +1,195 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, View, TouchableOpacity } from 'react-native';
+import { ScrollView, View, TouchableOpacity,Text } from 'react-native';
 import PropTypes from 'prop-types';
-
 import styles from './styles';
+import {
+  Menu,
+  MenuOptions,
+  MenuOption,
+  MenuTrigger,
+  renderers
+} from 'react-native-popup-menu';
 
-const HeatMapBlock = ({ size, value, index, colors, colorsPercentage, maximumValue, onBlockPress, style }) => {
-  const valuePercentage = value/maximumValue * 100;
+import {heatmap_green_dark,heatmap_green_medium,heatmap_green_light,heatmap_green_extra_light,heatmap_no_color,heatmap_red_dark,heatmap_red_medium,heatmap_red_light,heatmap_red_extra_light,black_color} from "../../../AppColors";
+
+const { Popover } = renderers;
+
+const HeatMapBlock = ({ size, value, index, colors,redcolors,colorsPercentage, maximumValue, minimumValue, onBlockPress, style }) => {
+
   let color;
 
-  for(let i = 0; i < colorsPercentage.length; i++) {
-    if(valuePercentage >= colorsPercentage[i])
-      color = colors[i];
-    else
-      break;
+  if(value.value >= 0)
+  {
+      const valuePercentage = value.value/maximumValue.value * 100;    
+
+      for(let i = 0; i < colorsPercentage.length; i++) 
+      {  
+        if(valuePercentage >= colorsPercentage[i])    
+          color = colors[i];    
+        else      
+          break;
+      }
+  }
+  else
+  {
+      const valuePercentage = value.value/minimumValue.value * 100;
+
+      for(let i = 0; i < colorsPercentage.length; i++) 
+      {  
+        if(valuePercentage >= colorsPercentage[i])    
+          color = redcolors[i];    
+        else      
+          break;
+      }
   }
 
   if(!color)
     return null;
-
+  
   return (
-    <TouchableOpacity onPress={() => onBlockPress({ value, index })} style={[styles.heatMapBlock, { backgroundColor: color, width: size, height: size }, style]}>
-    </TouchableOpacity>
+
+      <Menu renderer={Popover} rendererProps={{ placement: 'top',anchorStyle: {marginRight:1,backgroundColor:color} }}>
+        <MenuTrigger>
+          <View onPress={() => onBlockPress({ value, index })} style={[styles.heatMapBlock, { backgroundColor: color, width: size, height: size }, style]}>
+              <View style={{flex:1,borderWidth:0}}>
+                <View style={{flex:1,borderWidth:0,alignItems:'center',justifyContent:'flex-end'}}>
+                    <Text style={{fontSize:13,fontFamily:'Montserrat-Regular',color:black_color}}>
+                      {value.name.length > 10 ? `${value.name.substring(0, 5)}...`:value.name}  
+                    </Text>
+                </View>
+                <View style={{flex:1,borderWidth:0,alignItems:'center',justifyContent:"flex-start"}}>
+                    <Text style={{fontSize:13,fontFamily:'Montserrat-Regular',color:black_color}}>{value.value}</Text>
+                </View>
+              </View>
+          </View>
+        </MenuTrigger>
+        <MenuOptions optionsContainerStyle={{minWidth:150,borderRadius:7,height:60,alignItems:'center',justifyContent:'center',backgroundColor:color}}>    
+            {/* <View style={{alignItems:'center',justifyContent:'center'}}>
+              <Text>Hello World</Text>
+            </View>                             */}
+            <MenuOption style={{borderTopWidth:0,borderLeftWidth:0,borderRightWidth:0,borderBottomWidth:0}}>
+                <View style={{borderWidth:0,padding:3,minWidth:130,alignItems:'center',justifyContent:'center'}}>
+                    <Text style={{fontSize:13,fontFamily:'Montserrat-SemiBold',color:black_color}}>{value.name}</Text>
+                </View>
+                <View style={{borderWidth:0,padding:3,minWidth:130,alignItems:'center',justifyContent:'center'}}>
+                  <Text style={{fontSize:13,fontFamily:'Montserrat-Regular',color:black_color}}>{value.value}</Text>
+                </View>                                
+            </MenuOption>
+        </MenuOptions>
+      </Menu>
+
+    // <TouchableOpacity onPress={() => onBlockPress({ value, index })} style={[styles.heatMapBlock, { backgroundColor: color, width: size, height: size }, style]}>
+    //   <View style={{flex:1,borderWidth:0}}>
+    //     <View style={{flex:1,borderWidth:0,alignItems:'center',justifyContent:'flex-end'}}>
+    //       <Text style={{fontSize:13,fontFamily:'Montserrat-Regular'}}>
+    //           {value.name.length > 10 ? `${value.name.substring(0, 5)}...`:value.name}  
+    //       </Text>
+    //     </View>
+    //     <View style={{flex:1,borderWidth:0,alignItems:'center',justifyContent:"flex-start"}}>
+    //       <Text style={{fontSize:13,fontFamily:'Montserrat-Regular'}}>{value.value}</Text>
+    //     </View>
+    //   </View>
+    // </TouchableOpacity>
   );
 }
 
 const HeatMapColumn = ({ children }) => (
-  <View style={styles.heatMapColumn}>
+  <View style={[styles.heatMapColumn,{flexDirection:'row'}]}>
     { children }
   </View>
 );
 
-const HeatMap = ({ numberOfLines, values, indexStart, colors, colorsPercentage, maximumValue, blocksSize, onBlockPress, blocksStyle }) => {
-  const [maxValue, setMaxValue] = useState(maximumValue);
+const HeatMap = ({ numberOfLines, values, indexStart, colors,redcolors,colorsPercentage, maximumValue, blocksSize, onBlockPress, blocksStyle }) => {
+  // const [maxValue, setMaxValue] = useState(maximumValue);
+  // const [minValue, setMinValue] = useState(maximumValue);
+
+  const [maxValue, setMaxValue] = useState(0);
+  const [minValue, setMinValue] = useState(0);
   
   useEffect(() => {
-    setRelativeMaxValue();
+   // setRelativeMaxValue();
+   //setRelativeMinValue();
+   setRelativeMaxMinValue();
+
   }, []);
 
-  const setRelativeMaxValue = () => {
-    if(maxValue !== 'relative') return;
-    let max = 1;
+  // const setRelativeMaxValue = () => {
 
-    for(let i = 0; i < values.length; i++) {
-      if(values[i] > max)
-        max = values[i];
+  //   if(maxValue !== 'relative') return;
+  //   let max = 1;
+
+  //   for(let i = 0; i < values.length; i++) {
+
+  //     if(values[i] >= 0)
+  //     {
+  //       if(values[i] > max)
+  //         max = values[i];
+  //     }      
+  //   }
+
+  //   setMaxValue(max);
+  // }
+
+  // const setRelativeMinValue = () => {
+
+  //   if(minValue !== 'relative') return;
+  //   let min = -1;
+
+  //   for(let i = 0; i < values.length; i++) {
+
+  //     if(values[i] < 0)
+  //     {
+  //       if(values[i] < min)
+  //         min = values[i];
+  //     }      
+  //   }
+
+  //   setMinValue(min);
+  // }
+
+
+  const setRelativeMaxMinValue = () => {
+
+    let postiveArray  = [];
+    let negativeArray = [];
+
+    for(let i = 0; i < values.length; i++) 
+    {         
+        if(values[i].value >= 0)
+        {
+            postiveArray.push(values[i]);
+        }
+        else
+        {
+            negativeArray.push(values[i]);
+        }    
     }
 
-    setMaxValue(max);
+    postiveArray = postiveArray.sort((a,b)=>{
+      return a.value-b.value;
+    })
+
+    negativeArray = negativeArray.sort((a,b)=>{
+      return b.value-a.value;
+    })        
+
+    let maxValue = postiveArray[postiveArray.length-1];
+    let minValue = negativeArray[negativeArray.length-1];
+
+    setMaxValue(maxValue);
+    setMinValue(minValue)    
   }
+
 
   const generateBlocks = atualBlock => {
     const blocks = [];
     for(let j = 0; j < numberOfLines; j++)
-      blocks.push(<HeatMapBlock key={Math.random()} style={blocksStyle} size={blocksSize} index={j + atualBlock + indexStart} value={values[j + atualBlock]} colors={colors} colorsPercentage={colorsPercentage} onBlockPress={onBlockPress} maximumValue={maxValue} />);
+    {
+       if(values[j + atualBlock])
+       {
+          blocks.push(<HeatMapBlock key={Math.random()} style={blocksStyle} size={blocksSize} index={j + atualBlock + indexStart} value={values[j + atualBlock]} colors={colors} redcolors={redcolors} colorsPercentage={colorsPercentage} onBlockPress={onBlockPress} maximumValue={maxValue} minimumValue={minValue} />);
+       }    
+    }    
     return blocks;   
   }
 
@@ -73,7 +210,7 @@ const HeatMap = ({ numberOfLines, values, indexStart, colors, colorsPercentage, 
   }
 
   return (
-    <ScrollView horizontal={true}>
+    <ScrollView /*horizontal={true}*/>
       { generateColumns() }
     </ScrollView>
   );
@@ -83,6 +220,7 @@ HeatMap.propTypes = {
   numberOfLines: PropTypes.number,
   values: PropTypes.array,
   colors: PropTypes.array,
+  redcolors: PropTypes.array,
   colorsPercentage: PropTypes.array,
   maximumValue: PropTypes.string,
   blocksSize: PropTypes.number,
@@ -95,7 +233,8 @@ HeatMap.propTypes = {
 HeatMap.defaultProps = {
   numberOfLines: 7,
   values: [],
-  colors: ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'],
+  colors: [heatmap_no_color, heatmap_green_extra_light, heatmap_green_light, heatmap_green_medium, heatmap_green_dark],
+  redcolors : [heatmap_no_color,heatmap_red_extra_light,heatmap_red_light,heatmap_red_medium,heatmap_red_dark],
   colorsPercentage: [0, 0.00001, 41, 60, 80],
   maximumValue: 'relative',
   blocksSize: 30,
